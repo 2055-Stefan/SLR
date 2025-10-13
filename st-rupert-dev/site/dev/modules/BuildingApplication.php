269089f2bf8ed0e298814cb5ade76761eb2f3fd2
Stefan Scheer <2055@htl.rennweg.at>
2025-10-13 20:46:12 +0200

    US6: Add BuildingApplication module class with 5 dynamic building previews

---
+++ st-rupert-dev/site/dev/modules/BuildingApplication.php
@@ -0,0 +1,21 @@
+<?php
+require_once "AbstractPortalModule.php";
+
+class BuildingApplication extends AbstractPortalModule {
+    private string $description;
+
+    public function __construct(string $title, string $source, string $description) {
+        parent::__construct($title, $source);
+        $this->description = $description;
+    }
+
+    public function render(): string {
+        return "
+        <article class='building-application'>
+            <h2>{$this->getTitle()}</h2>
+            <p>{$this->description}</p>
+            <small>Quelle: {$this->getSource()}</small>
+        </article>";
+    }
+}
+?>
